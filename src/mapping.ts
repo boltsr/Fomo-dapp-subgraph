788fb0f2e567bac4bb81c72dc3cece107f87740b
boltsr <skrullweb.dev@gmail.com>
2021-10-10 23:18:29 -0400

    update itemListed mapping

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,13 +1,28 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts"
 
 import { FomoMarketplace, ItemCanceled, ItemListed, ItemSold, ItemUpdated, OfferCanceled, OfferCreated, OwnershipTransferred, UpdatePlatformFee, UpdatePlatformFeeRecipient } from "../generated/FomoMarketplace/FomoMarketplace"
 import { ListedItem } from "../generated/schema"
 
 export function handleItemCanceled(event: ItemCanceled): void {
+  
 }
 
 export function handleItemListed(event: ItemListed): void {
-  
+
+  let id: string = ListedItem.length.toString();  
+  let newItem = ListedItem.load(id);
+  if (newItem === null) {
+    newItem = new ListedItem(id);
+    newItem.id =(ListedItem.length+1).toString();
+    newItem.allowedAddress = event.params.allowedAddress;
+    newItem.count = BigInt.fromI32(ListedItem.length+1);
+    newItem.createdAtTimestamp = event.block.timestamp;
+    newItem.tokenId = event.params.tokenId;
+    newItem.quantity = event.params.quantity;
+    newItem.pricePerItem = BigDecimal.fromString(event.params.pricePerItem.toString());
+    newItem.isPrivate = event.params.isPrivate;
+    newItem.nft = event.params.nft;
+  }
 }
 
 export function handleItemSold(event: ItemSold): void {}
